de01593c66aa6300964de493a8129a55e635598a
lior Shine <shinelior@gmail.com>
2020-02-27 17:09:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 # Your code goes here!
-class Dog 
+class Dog
+  attr_accessor(:name)
   def initialize
     @name = name
     
   end
   
-  def name=(the_dog_name)
-    @name = the_dog_name 
-  end
+  #def name=(the_dog_name)
+    #@name = the_dog_name 
+ # end
     
   
 end
